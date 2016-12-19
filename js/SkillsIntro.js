0cf0d3d604698c81d9f1ef068d6296c76244e952
legendemil <emilpausz@gmail.com>
2016-12-19 16:20:53 +0100

    add heart logo and change styles for skills-intro

--- js/SkillsIntro.js
+++ js/SkillsIntro.js
@@ -1,15 +1,17 @@
 import React from "react";
 
+import HeartLogo from './Logos/HeartLogo';
+
 export default class SkillsIntro extends React.Component {
 
     render() {
         return(
             <section className="skills-intro">       
                 <h2>
-                I use HTML, CSS, JS to create [<span className="lighter">awesome</span>] webapps <span className="lighter text-smallest">Which you love</span>
+                I use <br /> HTML, CSS, JS <br /> to create [<span className="lighter">awesome</span>] webapps <span className="lighter text-smallest">Which you <HeartLogo /></span> 
                 </h2>  
                 
-                <h3 className="lighter text-smaller">&amp; <br /> I know some PHP and Databases</h3> 
+                <h3 className="lighter text-smaller">Pss... <br /> <br /> I know some PHP and Databases</h3> 
             </section> 
         )
     }
