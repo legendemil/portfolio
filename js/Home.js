1e642e6c7568e3c5592f7d08a6f53b242944fa67
legendemil <emilpausz@gmail.com>
2016-12-25 21:40:21 +0100

    add temporarly section separator

--- js/Home.js
+++ js/Home.js
@@ -19,7 +19,10 @@ export default class Home extends React.Component {
                     <HomeAbout 
                         headingText={"About me"} 
                         text={"I'm a Front-End Developer from Poland. Why I love coding? Because It's awesome!"}/>
-                </div>        
+                </div> 
+
+                <div className="section__separator">
+                </div>       
                 
             </section> 
         )
