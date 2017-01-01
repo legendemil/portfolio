7f4ffc96c6482bdca44e9191a6068e8ed9ca1526
legendemil <emilpausz@gmail.com>
2017-01-01 14:18:04 +0100

    add some visual effects

--- js/Home.js
+++ js/Home.js
@@ -13,8 +13,12 @@ export default class Home extends React.Component {
                 <PageHeader />
 
                 <div className="row">
-                    <HomeHeading 
-                    text={"Hi, I'm Emil"}/>
+                    <div className="col col--6">
+                        <HomeHeading 
+                            text={"Hi, I'm Emil"}/>
+                         <p className="home__caption">Nice to meet you</p>
+                    </div>
+                    
                     <HomeAbout 
                         headingText={"About me"} 
                         text={"I'm a Front-End Developer from Poland. Why I love coding? Because It's awesome!"}/>
